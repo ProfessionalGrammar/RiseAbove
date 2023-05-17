29a88fad1c7e57c2e5ade9b5c0f1caba88e175dc
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-17 09:47:02 -0400

    Update UI.java

--- UI.java
+++ UI.java
@@ -2,7 +2,15 @@
 public class UI {
   GameManager gm;
   JFrame window;
+  
   public UI(GameManager g){
     gm = g;
+  }
+  
+  public void createWindow (){
+    window = new JFrame ();
+    window.setSize();
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setLayout(null);
   }
 }

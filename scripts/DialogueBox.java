f40fa338af11f7dceb4550595caec991592bff18
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-17 10:07:00 -0400

    Update DialogueBox.java

--- scripts/DialogueBox.java
+++ scripts/DialogueBox.java
@@ -1,1 +1,19 @@
-
+public class DialogueBox {
+  String message;
+  int x = 0;
+  int y;
+  int width;
+  int height;
+  
+  public DialogueBox(String msg, UI ui){
+    y = ui.window.getHeight() / 5;
+    width = ui.window.getWidth();
+    height = ui.window.getHeight() - y;
+  }
+  
+  public void draw(Graphics g){
+    g.setColor(color.black);
+    g.fillRect(x, y, width, height);
+    g.drawString()
+  }
+}

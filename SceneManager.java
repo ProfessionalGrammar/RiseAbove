c0619190fa5425771fc247a0bcba69a9859d112b
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-22 23:37:55 -0400

    Create SceneManager.java

---
+++ SceneManager.java
@@ -0,0 +1,10 @@
+public class SceneManager {
+  LevelManager lm = new LevelManager (this);
+  public SceneManager(){
+    
+  }
+  
+  public static void main(String[] args){
+    new SceneManager();
+  }
+}

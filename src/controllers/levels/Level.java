233945fdbb22e725f29d2d1a91a1d22ef19d653b
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-25 10:11:06 -0400

    Create Level.java

---
+++ src/controllers/levels/Level.java
@@ -0,0 +1,57 @@
+public class Level {
+  private GameState gameState;
+  private GameManager gm;
+  private UI window;
+  
+  public Level (){
+    gameState = new GameState();
+    window = new UI (this);
+  }
+  
+  public void startLevel() {
+        // Perform initialization tasks
+        initializeGame();
+
+        // Start the game loop
+        while (!gameState.isGameOver()) {
+            // Process user input
+            processInput();
+
+            // Update game state
+            updateGameState();
+
+            // Render game view
+            render();
+        }
+
+        // Perform cleanup tasks
+        cleanUp();
+    }
+
+    /* We might want to create all this code as separate classes instead of methods*/
+    private void initializeGame() {
+        // Initialize game state, load assets, set up initial game objects, etc.
+        gameState.setLevel(1);
+    }
+
+    private void processInput() {
+        // Handle user input events such as mouse clicks, key presses, etc.
+        // Check user actions and update game state accordingly
+        // Handle interactive events and outcomes
+    }
+
+    private void updateGameState(GameState g) {
+        // Update game state based on user input, game logic, etc.
+        // Manage level and scene progression based on game state and user actions
+        // Determine outcomes based on user actions and update game state accordingly    
+        gameState = g;
+    }
+
+    private void render() {
+        // Render the game view based on the current game state
+    }
+
+    private void cleanUp() {
+        // Perform cleanup tasks such as releasing resources, saving game state, etc.
+    }
+}

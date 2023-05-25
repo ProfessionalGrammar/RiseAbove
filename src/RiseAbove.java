ca3136d9da6fcb8ace99d59f61cde18ac2ba9578
absoluteyeezies <84931756+absoluteyeezies@users.noreply.github.com>
2023-05-25 09:31:48 -0400

    Update RiseAbove.java

--- src/RiseAbove.java
+++ src/RiseAbove.java
@@ -1,1 +1,30 @@
 
+import java.awt.*;
+import javax.swing.*;
+
+public class RiseAbove {
+   public static JFrame frame;
+   
+   boolean gameRun = true;
+
+   public DriverClass() {
+      frame = new JFrame("Rise Above Mental Health Simulator");
+   }
+
+   public static void main(String[] args) {
+      DriverClass defaultRun = new DriverClass();
+      SwingUtilities.invokeLater(() -> new SplashScreen(frame));
+      
+      
+      /*
+      while(gameRun){
+       
+         new MainMenu(frame);
+      
+      }
+      
+      */
+      
+      
+   }
+}

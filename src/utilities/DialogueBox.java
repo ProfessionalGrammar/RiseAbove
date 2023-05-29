2ea3cf2631cbf08a19e76f50031c42855fa97278
TristanKo <133880235+TristanKo@users.noreply.github.com>
2023-05-29 10:34:13 -0400

    Update DialogueBox.java

--- src/utilities/DialogueBox.java
+++ src/utilities/DialogueBox.java
@@ -12,6 +12,7 @@ public class DialogueBox extends JPanel{
   public void draw(Graphics g){
     g.setColor(color.black);
     g.drawRect(5, 605, 1390, 190, 25, 25);
+    //g.drawRect(30, 600, 1340, 170);
     g.fillRect(0, 600, 1400, 200);
     g.drawString()
   }

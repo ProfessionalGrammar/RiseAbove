8c0d790e496d9c0ee3eb4136f595afcc22dc19c2
TristanKo <133880235+TristanKo@users.noreply.github.com>
2023-05-30 09:31:41 -0400

    Update DialogueBox.java

--- src/utilities/DialogueBox.java
+++ src/utilities/DialogueBox.java
@@ -7,13 +7,28 @@ public class DialogueBox extends JPanel{
   
   public DialogueBox(String txt){
     text = txt;
+    
+    messageText = new JTextArea(text);
+    messageText.setBounds(75, 550, 1250, 200);
+    messageText.setBackground(Colour.black);
+    messageText.setForeground(colour.white);
+    messageText.setEditable(false);
+    messageText.setLineWrap(true);
+    messageText.setWrapStyleWord(true);
+    messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 30));
+    window.add(messageText);
+      
+      
   }
   
-  public void draw(Graphics g){
+  
+  
+ /* public void draw(Graphics g){
     g.setColor(color.black);
     g.drawRect(5, 605, 1390, 190, 25, 25);
     //g.drawRect(30, 600, 1340, 170);
     g.fillRect(0, 600, 1400, 200);
     g.drawString()
-  }
+  }*/
+  
 }

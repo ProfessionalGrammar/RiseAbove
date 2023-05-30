e80da9532ac0fb77b0564a56e135c87644717d93
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-30 09:49:37 -0400

    Update GameState.java

--- src/models/GameState.java
+++ src/models/GameState.java
@@ -1,8 +1,8 @@
 
 public class GameState {
-    private int score;
-    private boolean isGameOver;
-    private boolean is2D;
+    private static int score;
+    private static boolean isGameOver;
+    private static boolean is2D;
     
     public GameState() {
         score = 0;
@@ -10,27 +10,31 @@ public class GameState {
         is2D = false;
     }
 
-    public int getScore() {
+    public static int getScore() {
         return score;
     }
 
-    public void increaseScore(int points) {
+    public static void increaseScore(int points) {
         score += points;
     }
 
-    public boolean isGameOver() {
+    public static boolean isGameOver() {
         return isGameOver;
     }
 
-    public void setGameOver(boolean gameOver) {
+    public static void setGameOver(boolean gameOver) {
         isGameOver = gameOver;
     }
     
-    public boolean is2D() {
+    public static boolean is2D() {
         return is2D;
     }
     
-    public void set2D(boolean 2D) {
+    public static void set2D(boolean 2D) {
         is2D = 2D;
+    }
+    
+    public static void updateState(GameState curState){
+        this = curState;
     }
 }

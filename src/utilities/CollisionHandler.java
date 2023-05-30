7278635b1e92524746bf3c391a6f9ce81700834f
TristanKo <133880235+TristanKo@users.noreply.github.com>
2023-05-30 09:50:51 -0400

    Update CollisionHandler.java

--- src/utilities/CollisionHandler.java
+++ src/utilities/CollisionHandler.java
@@ -1,5 +1,12 @@
 
 public class CollisionHandler {
- 
-  
+
+		Rectangle ---Rect = new Rectangle(x_---,y_---,100,65); //making a rectangle on the---
+		Rectangle --Rect    = new Rectangle(x_---,y_--,45,67); //making a rectangle on ---
+		
+		if(---Rect.intersects(--Rect)){
+
+		}
+	}//end collision detection
+
 }

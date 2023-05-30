e3599e543f00487e6739b27474dc87aa28348b25
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-30 14:08:48 -0400

    Update 2DMovement.java

--- src/views/2DMovement.java
+++ src/views/2DMovement.java
@@ -36,9 +36,23 @@ public class 2DMovement extends JPanel implements Runnable {
   }
   
   public void run() {
+    double drawInterval = 1000000000/FPS;
+    double delta = 0;
+    long lastTime = System.nanoTime();
+    long currentTime;
+    
     while(gameThread != null){
-      update(); //Update info about character's position
-      repaint(); //Repaint the screen
+      currentTime = System.nanoTime();
+      
+      delta += (currentTime - lastTime) / drawInterval;
+      
+      lastTime = currentTime;
+      
+      if(delta >= 1){
+        update(); //Update info about character's position
+        repaint(); //Repaint the screen
+        delta--;
+      }
     }
   }
   

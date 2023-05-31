f9bee6bdd353a35de6b4efd9481ad7f8613ba197
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-31 10:46:44 -0400

    Update Minigame.java

--- src/utilities/Minigame/Minigame.java
+++ src/utilities/Minigame/Minigame.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MiniGame extends JPanel {
-    private JFrame frame;
     private JPanel panel;
     private JLabel questionLabel;
     private JButton optionButton1;
@@ -13,13 +12,7 @@ public class MiniGame extends JPanel {
     public String option1;
     public String option2;
 
-    public MiniGame(String question, String option1, String option2) {
-        frame = new JFrame("minigame");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1400, 800);
-
-        setLayout(new FlowLayout());
-        
+    public MiniGame(String question, String option1, String option2) {        
         panel = new JPanel();
         questionLabel = new JLabel(question);
         panel.add(questionLabel);
@@ -34,7 +27,6 @@ public class MiniGame extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(panel, "Correct!");
-                frame.dispose();
             }
         });
 
@@ -42,19 +34,15 @@ public class MiniGame extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(panel, "Incorrect!");
-                frame.dispose();
             }
         });
-
-        frame.add(panel);
-        frame.setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+   }
+   
+  public void display(){
+   SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new MiniGame("hello", "one", "two");
             }
-        });
-    }
+        });   
+  }
 }

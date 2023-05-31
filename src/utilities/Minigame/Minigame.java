8fa445891ccf6259aa62ebe6404852c4eccfa1af
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-31 11:05:30 -0400

    Update Minigame.java

--- src/utilities/Minigame/Minigame.java
+++ src/utilities/Minigame/Minigame.java
@@ -5,41 +5,34 @@ import java.awt.event.ActionListener;
 
 public class MiniGame extends JPanel {
     private JPanel panel;
-    private JLabel questionLabel;
-    private JButton optionButton1;
-    private JButton optionButton2;
     public String question;
-    public String option1;
-    public String option2;
+    private JLabel questionLabel;
+    private JButton[] optionButtons;
+    public String[] options;
+    public boolean[] isCorrect;
     public GameState state;
 
-    public MiniGame(String question, String option1, String option2) {        
+    public MiniGame(String question, JButton[] buttons, String[] opts, boolean[] corr) {        
         panel = new JPanel();
         questionLabel = new JLabel(question);
         panel.add(questionLabel);
-
-        optionButton1 = new JButton(option1);
-        optionButton2 = new JButton(option2);
-
-        panel.add(optionButton1);
-        panel.add(optionButton2);
-
-        optionButton1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(panel, "Correct!");
-                state.answer(true);
-            }
-        });
-
-        optionButton2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JOptionPane.showMessageDialog(panel, "Incorrect!");
-                state.answer(false);
-            }
-        });
+        optionButtons = buttons;
+        options = opts;
+        isCorrect = corr;
+        
+        for(int i = 0; i < optionButtons.length; i++){
+            optionButtons[i] = new JButton(options[i])
+            panel.add(optionButtons[i]);
+            optionButton[i].addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    JOptionPane.showMessageDialog(panel, "Incorrect!");
+                    state.answer(false);
+                }
+            });
+        }
+        
    }
+   
    
   public void display(){
    SwingUtilities.invokeLater(new Runnable() {

1ffc101e3d37d6141ccf8d93c18e18b3f8e6d045
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-30 21:39:29 -0400

    Update GameObject.java

--- src/models/GameObject.java
+++ src/models/GameObject.java
@@ -2,9 +2,34 @@
 public class GameObject {
  private int x;
  private int y;
+ private int speed;
  public GameObject (int objx, int objy, int objWidth, int objHeight) {
   x = objx;
   y = objy;
   
+ }
+ 
+ public int getX () {
+  return x;
+ }
+ 
+ public int getY () {
+  return y;
+ }
+ 
+ public int getSpeed () {
+  return speed;
+ }
+ 
+ public void setX (int curX) {
+  x = curX;
+ }
+ 
+ public void setY (int curX) {
+  x = curX;
+ }
+ 
+ public void setSpeed (int spd) {
+  speed = spd;
  }
 }

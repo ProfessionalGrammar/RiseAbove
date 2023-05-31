1a6ed52e7d75aeb6a1d61d67aa1b6d894cf3468d
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-31 10:14:23 -0400

    Update RiseAbove.java

--- src/RiseAbove.java
+++ src/RiseAbove.java
@@ -4,27 +4,8 @@ import javax.swing.*;
 import java.util.*;
 
 public class RiseAbove {   
-
-   public RiseAbove() {
-   
-   }
-
    public static void main(String[] args) {
-      
-      boolean gameRun = true;
-      
-      SwingUtilities.invokeLater(() -> new SplashScreen()); // splash screen and game title page call
-      while (gameRun) {         
-         if (SplashScreen.splashScreenExit) {
-         
-            System.out.println(SplashScreen.splashScreenExit);
-         
-            new MainMenu();
-            SplashScreen.splashScreenExit = false;
-         
-         }
-      }
-     
-             
+      SplashScreen splash = new SplashScreen();
+      splash.display();
    }
 }

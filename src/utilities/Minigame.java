bbde38e3ba6bc311e354c4e17592a1b038e5719a
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-05-31 10:19:35 -0400

    Update Minigame.java

--- src/utilities/Minigame.java
+++ src/utilities/Minigame.java
@@ -1,4 +1,23 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
 
-public class Minigame {
+public class Minigame extends JPanel{  
+  private static JTextArea messageText;
   
+  public Minigame(String txt){
+    messageText = new JTextArea(txt);
+    messageText.setBounds(75, 550, 1250, 200);
+    messageText.setBackground(Colour.black);
+    messageText.setForeground(colour.white);
+    messageText.setEditable(false);
+    messageText.setLineWrap(true);
+    messageText.setWrapStyleWord(true);
+    messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 30));
+    window.add(messageText);
+  }
+  
+  public void setText(String txt){
+   messageText.setText(txt); 
+  }
 }

c289eabf8743647d05ed33ac60cc3d161f0adeea
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-06-01 09:28:07 -0400

    Update l1scene2.java

--- src/scenes/lv1/l1scene2.java
+++ src/scenes/lv1/l1scene2.java
@@ -1,5 +1,5 @@
 
-public class l1scene2 extends JPanel{
+public class l1scene2 extends Scene{
    public void paint(Graphics g) {
     g.drawString("l1scene2, arrives at therapy centre", 0, 0);
    }

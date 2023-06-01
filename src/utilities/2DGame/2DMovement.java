7bb591d126eee47e7224f3b67ab2b7b062d40ed5
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-06-01 09:00:43 -0400

    Update 2DMovement.java

--- src/utilities/2DGame/2DMovement.java
+++ src/utilities/2DGame/2DMovement.java
@@ -2,14 +2,6 @@ java.awt.*;
 java.swing.*;
 
 public class 2DMovement extends JPanel implements Runnable {
-  final int originalTileSize = 16; //16x16 tiles
-  final int scale = 3;
-  
-  final int tileSize = originalTileSize * scale; //48x48 tiles
-  final int maxScreenCol = 16;
-  final int maxScreenRow = 12;
-  final int screenWidth = tileSize * maxScreenCol;
-  final int screenHeight = tileSize * maxScreenRow;
   
   int FPS = 60;
   

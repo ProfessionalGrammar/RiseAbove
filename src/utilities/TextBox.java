2fed2546fe2efa8e98a095fbd9bd687e6ceed3a1
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-06-02 10:21:24 -0400

    Update TextBox.java

--- src/utilities/TextBox.java
+++ src/utilities/TextBox.java
@@ -1,21 +1,32 @@
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
 
-public class TextBox extends JTextArea{  
-  
-  public TextBox(String txt, int x, int y, int width, int height){
-    this = new JTextArea(txt);
+public class TextBox extends JTextArea {
+
+  public TextBox(String txt, int x, int y, int width, int height) {
+    super(txt);
     this.setBounds(x, y, width, height);
-    this.setBackground(Colour.black);
-    this.setForeground(colour.white);
+    this.setBackground(new Color(0, 0, 0, 128)); // Set translucent background
+    this.setForeground(Color.WHITE);
     this.setEditable(false);
     this.setLineWrap(true);
     this.setWrapStyleWord(true);
     this.setFont(new Font("Book Antiqua", Font.PLAIN, 30));
   }
-  
-  public void setText(String txt){
-   this.setText(txt); 
+
+  @Override
+  protected void paintComponent(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g.create();
+    g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5f)); // Set transparency level
+    g2d.setColor(getBackground());
+    g2d.fillRect(0, 0, getWidth(), getHeight());
+    super.paintComponent(g2d);
+    g2d.dispose();
+  }
+
+  public void setText(String txt) {
+    super.setText(txt);
   }
 }
+

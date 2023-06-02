cc5db3216593e2b7c9894d0e9590e1b2c3064527
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-06-02 09:55:11 -0400

    Create Buttons.java

---
+++ src/utilities/Buttons.java
@@ -0,0 +1,17 @@
+
+public class Buttons {
+  int x;
+  int y;
+  int width;
+  int height;
+  String function;
+  
+  public Buttons (int x, int y, int width, int height, String function){
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.function = function;
+    
+  }
+}

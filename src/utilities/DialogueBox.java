6c1556747a5ae650288b50ca942c7619e9049687
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-06-02 09:37:04 -0400

    Update DialogueBox.java

--- src/utilities/DialogueBox.java
+++ src/utilities/DialogueBox.java
@@ -2,22 +2,20 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 
-public class DialogueBox extends JPanel{  
-  private static JTextArea messageText;
+public class DialogueBox extends JTextArea{  
   
   public DialogueBox(String txt){
-    messageText = new JTextArea(txt);
-    messageText.setBounds(75, 550, 1250, 200);
-    messageText.setBackground(Colour.black);
-    messageText.setForeground(colour.white);
-    messageText.setEditable(false);
-    messageText.setLineWrap(true);
-    messageText.setWrapStyleWord(true);
-    messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 30));
-    window.add(messageText);
+    this = new JTextArea(txt);
+    this.setBounds(75, 550, 1250, 200);
+    this.setBackground(Colour.black);
+    this.setForeground(colour.white);
+    this.setEditable(false);
+    this.setLineWrap(true);
+    this.setWrapStyleWord(true);
+    this.setFont(new Font("Book Antiqua", Font.PLAIN, 30));
   }
   
   public void setText(String txt){
-   messageText.setText(txt); 
+   this.setText(txt); 
   }
 }

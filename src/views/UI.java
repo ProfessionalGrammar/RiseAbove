6be43bc1af2c326e33bf24d19c594062d148d1c0
ProfessionalGrammar <46902609+ProfessionalGrammar@users.noreply.github.com>
2023-06-03 19:43:30 -0400

    Update UI.java

--- src/views/UI.java
+++ src/views/UI.java
@@ -1,27 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class UI {
+public class UI extends JFrame{
   private static GameManager gm;
-  private static JFrame window;
   private static JLayeredPane curScene;
   
   public UI(GameManager g){
     gm = g;
     createWindow();
-    window.setVisible(true);
+    this.setVisible(true);
   }
   
   public static void createWindow (){
-    window = new JFrame ("Rise Above");
-    window.setSize(1400, 800);
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    window.setResizable(false);
-    window.setLayout(null);
-  }
-  
-  public static void add(JLayeredPane scene) {
-    curScene = scene;
-    window.add(scene);
+    this = new JFrame ("Rise Above");
+    this.setSize(1400, 800);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.setLayout(null);
   }
 }

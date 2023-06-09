4d901f355f9920c9c4188c742a05660108d32c9a
absoluteyeezies <84931756+absoluteyeezies@users.noreply.github.com>
2023-06-09 10:18:20 -0400

    Update GameManager.java

--- src/controllers/GameManager.java
+++ src/controllers/GameManager.java
@@ -1,7 +1,8 @@
 package controllers;
+
+
 import models.*;
 import views.*;
-import java.lang.reflect.InvocationTargetException;
 import views.scenes.*;
 import views.scenes.lv1.*;
 import utilities.*;
@@ -10,82 +11,87 @@ import javax.swing.*;
 import java.util.*;
 
 public class GameManager {
-  private GameState gameState;
-  private InputHandler handler;
-  private Scene scene;
+   private GameState gameState;
+   private InputHandler handler;
+   private Scene scene;
       
-  public GameManager(){
-    gameState = new GameState();
-    handler = new InputHandler(gameState, this);
-  }
+   public GameManager(){
+      gameState = new GameState();
+      handler = new InputHandler(gameState, this);
+   }
   
    public void startGame() {
-        GameLoop lp = new GameLoop();
-        Thread loop = new Thread(lp);
-        loop.start();
-
-        try {
-            SwingUtilities.invokeAndWait(() -> new SplashScreen());
-        } catch (InterruptedException | InvocationTargetException e) {
-            e.printStackTrace();
-        }
-
-        scene = new MainMenu();
-        scene.display(handler);
-    }
+      GameLoop lp = new GameLoop();
+      Thread loop = new Thread(lp);
+      loop.start();
+      
+      SwingUtilities.invokeLater(() -> new SplashScreen());
+      System.out.println(SplashScreen.endS1);
+        
+      boolean tempWhile = true;
+      
+      while (tempWhile) {
+        
+        
+      
+      
+         if (SplashScreen.endS1 == true){
+            tempWhile = false;
+            scene = new MainMenu();
+            scene.display(handler);
+         }
+         
+         System.out.println(SplashScreen.endS1);
+      }
+   }
   
-  public void changeScene(Scene newScene){
+   public void changeScene(Scene newScene){
       scene.endScene();
       newScene.display(handler);
       scene = newScene;
-  }
+   }
   
-  public void getNextScene(){
-      System.out.println("setting next");
-      changeScene(scene.getNextScene());
-  }
-  
-  public void nextText(){
-   scene.nextText();
-  }
+   public void nextText(){
+      scene.nextText();
+   }
 
-  private void processInput() {
+   private void processInput() {
         // Handle user input events such as mouse clicks, key presses, etc.
         // Check user actions and update game state accordingly
         // Handle interactive events and outcomes
-  }
+   }
 
-  private void updateGame() {
+   private void updateGame() {
         // Update game state based on user input, game logic, etc.
         // Manage level and scene progression based on game state and user actions
         // Determine outcomes based on user actions and update game state accordingly
       
-  }
+   }
 
-  private void render() {
+   private void render() {
         // Render the game view based on the current game state
-  }
+   }
 
-  private void endGame() {
+   private void endGame() {
         // Perform cleanup tasks such as releasing resources, saving game state, etc.
-        System.exit(0);
-  }
+      System.exit(0);
+   }
     
-  public class GameLoop implements Runnable {
-
-     @Override
-     public void run() {        
+   public class GameLoop implements Runnable {
+   
+      @Override
+      public void run() {        
          while (!gameState.isGameOver()) {
                
-             processInput();
-             updateGame();
-             render();
+            processInput();
+            updateGame();
+            render();
                
-             try {
-                 Thread.sleep(1000); // Pause the thread for 1 second
-             } catch (InterruptedException e) {
-                 e.printStackTrace();
-             }
+            try {
+               Thread.sleep(1000); // Pause the thread for 1 second
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
          }    
          
          endGame();    

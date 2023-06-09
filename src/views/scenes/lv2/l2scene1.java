bbf86085a50b8fc58d979942e49c2ee0907fc985
TristanKo <133880235+TristanKo@users.noreply.github.com>
2023-06-09 10:33:32 -0400

    Update l2scene1.java

--- src/views/scenes/lv2/l2scene1.java
+++ src/views/scenes/lv2/l2scene1.java
@@ -14,19 +14,34 @@ public class l2scene1 extends Scene{
    
    public void display (InputHandler handler) {
    
-      frame = new JFrame ("Rise Above Mental Health Simulator: Intro Scene");
+   frame = new JFrame ("Rise Above Mental Health Simulator: Minigame");
       frame.setSize(1400,800);
+      
+      dialogue = new String[] {"How have you been doing?", "Good to hear that.", "I love pizza.", "That's a good idea!", "Trying to get better!"};
+      dialogueBox = new TextBox(dialogue[0], 50, 600, 1300, 200);
+      dialogueCounter++;
+      
       JLayeredPane layeredPane = new JLayeredPane();
       draw.setBounds(0, 0, 1400, 800);  
       layeredPane.add(draw, Integer.valueOf(-1));
-            
+      
+      layeredPane.add(dialogueBox, Integer.valueOf(1));
+        
+        JPanel buttonPanel = new JPanel(); // JPanel to hold buttons
+        buttonPanel.setOpaque(false);
+        buttonPanel.setBounds(1250, 600, 100, 100);
+        GameButton nextButton = new GameButton("Next", "NextDialogue", 1250, 600, 100, 100, handler);
+        nextButton.setOpaque(true);
+        buttonPanel.add(nextButton);
+        layeredPane.add(buttonPanel, Integer.valueOf(2));
+        
         frame.add(layeredPane);
         frame.setVisible(true);
-        frame.addKeyListener(handler);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
-   }
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     }
 
-   
+   }
    
    public class DrawingBack extends JComponent {
    
@@ -37,6 +52,6 @@ public class l2scene1 extends Scene{
       g.setColor(Color.WHITE);
        g.fillRect(0,0,1400,800);
        }
-       
+       }
 }
 

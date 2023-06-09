e2c373cad8dabcfc4bbd90f7f91f92e338093fda
TristanKo <133880235+TristanKo@users.noreply.github.com>
2023-06-09 09:53:41 -0400

    Update l2scene1.java

--- src/views/scenes/lv2/l2scene1.java
+++ src/views/scenes/lv2/l2scene1.java
@@ -1,17 +1,36 @@
 package views.scenes.lv2;
 
+import views.scenes.*;
 import utilities.*;
-import views.scenes.*;
-import views.scenes.*;
 import java.awt.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class l2scene1 extends Scene{
    
-   public void display(InputHandler handler){
-   
+   JFrame frame;
+   DrawingBack draw = new DrawingBack();
+
+   public void display (InputHandler handler) {
+   frame = new JFrame ("Rise Above Mental Health Simulator: Quiz Minigame");
+      frame.setSize(1400,800);
+
+   JLayeredPane layeredPane = new JLayeredPane();
+      draw.setBounds(0, 0, 1400, 800);  
+      layeredPane.add(draw, Integer.valueOf(-1));
+
    }
    
-   public void paint(Graphics g) {
-     g.drawString("l2scene1", 0, 0);
-   }
+   public class DrawingBack extends JComponent {
+   
+      public DrawingBack () {
+      
+      }
+      public void paint(Graphics g){
+      g.setColor(Color.WHITE);
+       g.fillRect(0,0,1400,800);
+       }
+       }
+
 }
